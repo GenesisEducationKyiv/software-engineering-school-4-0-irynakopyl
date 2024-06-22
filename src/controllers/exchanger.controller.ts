ce6f358e2215e172d315ff36071ef49caa64440c
Iryna <iruna.kopyl@gmail.com>
2024-06-23 01:59:14 +0300

    conversion

--- src/controllers/exchanger.controller.ts
+++ src/controllers/exchanger.controller.ts
@@ -6,7 +6,7 @@ import ukrainianBankExchangeHandler from '../services/bank-exchange-handler';
 export async function getCurrentRate(req: Request, res: Response) {
   try {
     const currentRate = await new ExchangerService(ukrainianBankExchangeHandler).getCurrentRate();
-    return res.status(StatusCode.Success).json(currentRate);
+    return res.status(StatusCode.Success).json(Number(currentRate));
   } catch (error) {
     return res.status(StatusCode.InternalError).send(`Error getting current rate: ${JSON.stringify(error)}`);
   }
