c82707ef5919c89d17ac34de45cb7fb4ec8d45fe
Iryna <iruna.kopyl@gmail.com>
2024-07-24 01:19:58 +0300

    HW10 Observability

--- orchestrator/common/logger.service.ts
+++ orchestrator/common/logger.service.ts
@@ -6,6 +6,14 @@ export class logger {
   error(message: string): void {
     console.error(message);
   }
+
+  warn(message: string): void {
+    console.warn(message);
+  }
+
+  debug(message: string): void {
+    console.debug(message);
+  }
 }
 
 export default new logger();
